f46fc792f4de305570ed8a99f0f6516464c45308
CalumBradbury24 <52780868+CalumBradbury24@users.noreply.github.com>
2020-11-26 11:36:26 +0000

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,28 +1,25 @@
-//Probably dont need the post requests since this is handled in python?
 const router = require('express').Router();
 let Quiz = require('../models/quiz.model');
 
 
-//:id is a variable that is used to get an object by a given id
 router.route('/').get((req, res) => {
   Quiz.find()
-    .then(quiz => res.json(quiz))//Return reading in JSON 
+    .then(quiz => res.json(quiz))//Return in JSON 
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-//Endpoint for incoming POST requests on '/CurrentAirQuality' URL path for users
 router.route('/').post((req, res) => {
   const name = req.body.name;
-  const score = Number(req.body.score);//Cast to a number
+  const score = Number(req.body.score);
 
-  const newResult = new Quiz({//Create new instance of reading using given sensorID
+  const newResult = new Quiz({
     name,
     score
   });
 
-  newResult.save()//Save new reading to database
+  newResult.save()
     .then(() => res.json('Quiz result added!'))
     .catch(err => res.status(400).json('Error: ' + err));
 }); 
 
-module.exports = router;//export router
+module.exports = router;
